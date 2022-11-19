bcf61cb43e45d5cfdca880bd19521742838352ea
djzee5 <113630697+djzee5@users.noreply.github.com>
2022-11-19 15:06:18 -0500

    Create script.js

---
+++ music/scripts/script.js
@@ -0,0 +1,16 @@
+/*
+    Student Name: Samuel Ayoola
+    File Name: script.js
+    Date: 11/19/2022
+*/
+
+//Hamburger menu function
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
